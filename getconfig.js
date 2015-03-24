2a95b1c85d463ac5530997ff767690db561e0415
Nathan LaFreniere <quitlahok@gmail.com>
2015-03-24 16:03:53 -0700

    refactor, make it all way simpler

--- getconfig.js
+++ getconfig.js
@@ -1,82 +1,58 @@
-var ALCE = require('alce');
-var fs = require('fs');
-var path = require('path');
-var tryit = require('tryit');
-var env = process.env.NODE_ENV || 'dev';
-var colors = require('colors');
-var useColor = true;
-var silent = false;
-var devAliases = [
-    'development',
-    'dev',
-    'devel',
-    'develop'
-];
-var isDev = devAliases.indexOf(env) !== -1;
-var color, config, configRoot, configPath, altConfigPath;
+var Hoek = require('hoek');
+var Path = require('path');
 
+var internals = {};
+internals.devAliases = ['dev', 'devel', 'develop', 'development'];
 
-// set our color based on environment
-if (env === 'dev') {
-    color = 'red';
-} else if (env === 'test') {
-    color = 'yellow';
-} else if (env === 'production') {
-    color = 'green';
-} else {
-    color = 'blue';
-}
+internals.tryRequire = function (root, type) {
 
-// color
-function c(str, color) {
-    return useColor ? str[color] : str;
-}
-
-// build a file path to the config
-configRoot = process.env.GETCONFIG_ROOT || (require.main ? path.dirname(require.main.filename) : ".");
-configPath = configRoot + path.sep + env + '_config.json';
-
-tryit(function () {
-    config = fs.readFileSync(configPath, 'utf-8');
-});
-
-// if development also try other names
-if (!config && isDev) {
-    // we'll try until we get a config
-    devAliases.some(function (alias) {
-        altConfigPath = configRoot + path.sep + alias + '_config.json';
-        tryit(function () {
-            config = fs.readFileSync(altConfigPath, 'utf-8');
-        });
-        return !!config;
-    });
-}
-
-if (!config) {
-    console.error(c("No config file found for %s", 'red'), env);
-    console.error(c("We couldn't find anything at: %s", 'grey'), configPath);
-    config = "{}";
-}
-
-try {
-    // use ALCE to parse to allow comments/single quotes
-    config = ALCE.parse(config);
-    if (config.getconfig) {
-        if (config.getconfig.hasOwnProperty('colors')) useColor = config.getconfig.colors;
-        if (config.getconfig.hasOwnProperty('silent')) silent = config.getconfig.silent;
-    } else {
-        config.getconfig = {};
+    var path = Path.join(root, 'config', type);
+    if (root === './') {
+        path = root + path;
     }
-    config.getconfig.env = env;
 
-} catch (e) {
-    console.error(c("Invalid JSON file", 'red'));
-    console.error(c("Check it at:", 'grey') + c(" http://jsonlint.com", 'blue'));
-    throw e;
-}
+    var result;
+    try {
+        result = require(path);
+    }
+    catch (e) {
+        if (e.code === 'MODULE_NOT_FOUND') {
+            result = {};
+        }
+        else {
+            throw e;
+        }
+    }
 
-// log out what we've got
-if (!silent) console.log(c(c(env, color), 'bold') + c(' environment detected', 'grey'));
+    return result;
+};
 
-// export it
-module.exports = config;
+internals.init = function () {
+
+    var root = process.env.GETCONFIG_ROOT || (require.main ? Path.dirname(require.main.filename) : './');
+
+    var config = {};
+    var types = ['default'];
+    if (process.env.NODE_ENV) {
+        types.push(process.env.NODE_ENV);
+        config.getconfig = {
+            env: process.env.NODE_ENV
+        };
+    }
+    else {
+        types = types.concat(internals.devAliases);
+        config.getconfig = {
+            env: 'dev',
+            isDev: true
+        };
+    }
+    types.push('local');
+
+    for (var i = 0, il = types.length; i < il; ++i) {
+        Hoek.merge(config, internals.tryRequire(root, types[i]));
+    }
+
+    return config;
+};
+
+module.exports = internals.init();
--- tests/typical/app.js
+++ tests/typical/app.js
@@ -3,6 +3,7 @@ var config = require('../../getconfig.js');
 var assert = require('assert');
 var NODE_ENV = process.env.NODE_ENV || 'dev';
 
+console.log(config);
 if (NODE_ENV === 'dev') {
     assert.equal(config.getconfig.env, 'dev');
     assert.equal(config.testValue, 'dev-value');
